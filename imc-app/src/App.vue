bccfc0a768f4e6f0c66c920ae19e4c34ea12d5b8
OsmarNeto <osmar.neto23@gmail.com>
2022-09-30 00:20:33 -0300

    Implementação das conexões entre elementos

--- imc-app/src/App.vue
+++ imc-app/src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <h1>Cálculo IMC</h1>
   <h2>Digite seu peso e altura</h2>
+  <div class="userInput">
+    <span class="p-float-label">
+      <InputText id="input-weight" type="text" v-model="weight" />
+      <label for="input-weight">Peso</label>
+    </span>
+  </div>
+  <div class="userInput">
+     <span class="p-float-label">
+       <InputText id="input-height" type="text" v-model="height" />
+       <label for="input-height">Altura</label>
+     </span>
+  </div>
+  <div class="btn-calc">
+    <PrimeButton label="Calcular" @click="CalcularIMC"/>
+  </div>
 
-  <span class="p-float-label">
-	<InputText id="input-weight" type="text" v-model="weight" />
-	<label for="input-weight">Peso</label>
-  </span>
+  <div class="imc-msg">
+    <p>Seu IMC é: {{imc}}</p>
+  </div>
 
-  <span class="p-float-label">
-	<InputText id="input-height" type="text" v-model="height" />
-	<label for="input-height">Peso</label>
-  </span>
-  <PrimeButton label="Calcular" />
 </template>
 
 <script>
@@ -19,7 +28,14 @@ export default {
   data() {
     return{
       height: null,
-      weight: null
+      weight: null,
+      imc: null
+    }
+  },
+  methods:{
+    CalcularIMC: function (){
+      this.imc = (this.weight/(this.height**2)).toFixed(2);
+      console.log(this.imc);
     }
   }
 }
@@ -33,5 +49,11 @@ export default {
 /*  color: #2c3e50;*/
 /*}*/
 
+.userInput{
+  margin: 30px 10px;
+}
 
+PrimeButton{
+  margin: 0 10px;
+}
 </style>
