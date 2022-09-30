2774bcb9966ec8c56a3cd462e03908ddea6eacda
OsmarNeto <osmar.neto23@gmail.com>
2022-09-29 23:50:00 -0300

    Elementos input e vinculo com altura e peso

--- imc-app/src/App.vue
+++ imc-app/src/App.vue
@@ -2,19 +2,36 @@
   <h1>Cálculo IMC</h1>
   <h2>Digite seu peso e altura</h2>
 
+  <span class="p-float-label">
+	<InputText id="input-weight" type="text" v-model="weight" />
+	<label for="input-weight">Peso</label>
+  </span>
+
+  <span class="p-float-label">
+	<InputText id="input-height" type="text" v-model="height" />
+	<label for="input-height">Peso</label>
+  </span>
+  <PrimeButton label="Calcular" />
 </template>
 
 <script>
 export default {
-
+  data() {
+    return{
+      height: null,
+      weight: null
+    }
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
+/*#app {*/
+/*  font-family: Avenir, Helvetica, Arial, sans-serif;*/
+/*  -webkit-font-smoothing: antialiased;*/
+/*  -moz-osx-font-smoothing: grayscale;*/
+/*  color: #2c3e50;*/
+/*}*/
+
+
 </style>
